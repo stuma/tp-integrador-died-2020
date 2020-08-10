d6e0caaa2c97235c6b9c5ff7f35ef40499cc092f
CarlaMoreno593 <38229608+CarlaMoreno593@users.noreply.github.com>
2020-08-10 17:31:01 -0300

    "Actualización del pom"

---
+++ src/main/java/Test/TestPedido.java
@@ -0,0 +1,12 @@
+package Test;
+
+import org.junit.jupiter.api.Test;
+
+public class TestPedido {
+
+    @Test
+    public void printTest() {
+       System.out.println("Running JUNIT test");
+    }
+
+}
