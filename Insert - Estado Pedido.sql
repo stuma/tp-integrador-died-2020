61587ad89dea036363a26753740c7535408a0864
CarlaMoreno593 <38229608+CarlaMoreno593@users.noreply.github.com>
2020-08-10 19:18:13 -0300

    "Agrego Diagrama de Clases y los archivos SQL"

---
+++ Insert - Estado Pedido.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 10-08-2020 a las 22:01:59
+-- Versión del servidor: 5.7.26
+-- Versión de PHP: 7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tp-integrador-died-2020`
+--
+
+--
+-- Volcado de datos para la tabla `estadopedido`
+--
+
+INSERT INTO `estadopedido` (`id`, `descripcion`) VALUES
+(0, 'CREADA'),
+(1, 'PROCESADA'),
+(2, 'ENTREGADA'),
+(3, 'CANCELADA');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
